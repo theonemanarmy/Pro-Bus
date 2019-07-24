fb350523680e1e70a6fe97a629f7390c678a8fe4
theonemanarmy <juan_27angel@hotmail.com>
2019-07-24 15:09:18 -0500

    casi completa solo faltan formularios

--- js/custom.js
+++ js/custom.js
@@ -5,5 +5,23 @@ nav.addEventListener('click', function(){
 });
 
 $(document).ready(function(){
-    $('.owl-carousel').owlCarousel();
+    $('.owl-carousel').owlCarousel({
+        items : 1,
+        itemsDesktop : [1199,1],
+        itemsDesktopSmall : [980,1],
+        itemsTablet: [768,2],
+        itemsTabletSmall: false,
+        itemsMobile : [479,1],
+        singleItem : false,
+
+            // Navigation
+        navigation : true,
+        navigationText : ["prev","next"],
+        rewindNav : true,
+        scrollPerPage : false,
+
+        //Pagination
+        pagination : true,
+        paginationNumbers: false
+    });
 });
